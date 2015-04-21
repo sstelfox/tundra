f1ec6e3e2dc11be6ded48970bfb16d0bbb5d18c0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-04-21 14:25:32 -0400

    Tests covering access to the logger singleton

--- lib/stats_collector.rb
+++ lib/stats_collector.rb
@@ -7,4 +7,19 @@ require 'stats_collector/version'
 # module. This parent namespace also contains accessors for singleton instances
 # of utility sub-classes.
 module StatsCollector
+  # Access the singleton instance of the currently configured logger. If one
+  # isn't defined then this will initialize the logger to an appropriate one.
+  #
+  # @return [Object] An appropriate logger instance.
+  def self.logger
+    @logger ||= StatsCollector::Loggers::StandardLogger.new
+  end
+
+  # Replace the current singleton instance for the logger with the provided
+  # one.
+  #
+  # @param [Object] An appropriate logger instance.
+  def self.logger=(logger)
+    @logger = logger
+  end
 end
--- spec/stats_collector_spec.rb
+++ spec/stats_collector_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe(StatsCollector) do
   it 'has a version number' do
     expect(StatsCollector::VERSION).not_to be nil
   end
+
+  context 'logger singleton' do
+    let(:default_logger) { StatsCollector::Loggers::StandardLogger }
+
+    after(:each) do
+      # We need to reset the state of the logger singleton to ensure the tests
+      # run consistently and don't have a weird state afterwards.
+      described_class.instance_variable_set(:@logger, nil)
+    end
+
+    it 'initializes to a standard logger when one isn\'t set' do
+      expect(described_class.instance_variable_get(:@logger)).to be_nil
+      expect(described_class.logger).to be_instance_of(default_logger)
+      expect(described_class.instance_variable_get(:@logger)).to_not be_nil
+    end
+
+    it 'returns the same instance everytime' do
+      expect(described_class.logger).to be(described_class.logger)
+    end
+
+    it 'allows replacing the instance with another one' do
+      replacement = double
+      described_class.logger = replacement
+
+      expect(described_class.logger).to eq(replacement)
+    end
+  end
 end
