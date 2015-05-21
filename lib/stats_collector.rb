5106a57253b785bd2cd2b34a0cf5682ac614151e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-05-21 10:33:35 -0500

    Some minor code cleanup and fixing some rubocop offenses

--- lib/stats_collector.rb
+++ lib/stats_collector.rb
@@ -7,6 +7,10 @@ require 'stats_collector/version'
 # Parent namespace for this codebase, encapsulating everything within this
 # module. This parent namespace also contains accessors for singleton instances
 # of utility sub-classes.
+#
+# @!attribute [w] logger
+#   @return [Object] Replace the current singleton logger instance for the
+#     logger with the provided one.
 module StatsCollector
   # Access the singleton instance of the currently configured logger. If one
   # isn't defined then this will initialize the logger to an appropriate one.
@@ -16,11 +20,7 @@ module StatsCollector
     @logger ||= StatsCollector::Loggers::StandardLogger.new
   end
 
-  # Replace the current singleton instance for the logger with the provided
-  # one.
-  #
-  # @param [Object] logger An appropriate logger instance.
-  def self.logger=(logger)
-    @logger = logger
+  class << self
+    attr_writer :logger
   end
 end
