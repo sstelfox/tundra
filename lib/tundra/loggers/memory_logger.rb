cdd332a43999708bb4e769034f335f373c7a7d88
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-05-21 17:37:45 -0500

    More complete test coverage

--- lib/tundra/loggers/memory_logger.rb
+++ lib/tundra/loggers/memory_logger.rb
@@ -15,16 +15,29 @@ module Tundra
     class MemoryLogger
       include LevelShortcuts
 
+      # Takes a logger and logs all the stored messages to the provided logger
+      # instance. Once it's done dumping it empties the internal buffer to
+      # prevent them from being logged again.
+      #
+      # @param [#log] logger The logger instance to dump to.
+      def dump_to_logger(logger)
+        messages.each { |severity, message| logger.log(severity, message) }
+        messages.clear!
+      end
+
       # Initialize an instance of a MemoryLogger
       def initialize
         self.messages = RingBuffer.new(LOG_MEMORY_RING_SIZE)
       end
 
       # No Op, only here to maintain API compatibility with all the other
-      # loggers.
+      # loggers. Effectively this logger always operated in 'debug' mode so it
+      # will respond as such.
       #
       # @param [Array<Object>] _args Unused
+      # @return [:debug]
       def level(*_args)
+        :debug
       end
 
       alias_method :level=, :level
--- spec/tundra/loggers/memory_logger_spec.rb
+++ spec/tundra/loggers/memory_logger_spec.rb
@@ -1,6 +1,49 @@
 require 'spec_helper'
 require 'shared_examples/common_logger_examples'
 
-RSpec.describe(Tundra::Loggers::MemoryLogger) do
+RSpec.describe(::Tundra::Loggers::MemoryLogger) do
   it_behaves_like 'a logger'
+
+  it 'ignores attempts to change it\'s log level' do
+    expect { subject.level = :error }.to_not change { subject.level }
+  end
+
+  it 'operates as if it was in debug mode' do
+    expect(subject.level).to eq(:debug)
+  end
+
+  it 'stores log messages in the internal buffer' do
+    buffer_double = instance_double('::Tundra::RingBuffer')
+    message = 'Log message'
+
+    expect(subject).to receive(:messages).and_return(buffer_double)
+    expect(buffer_double).to receive(:<<).with([:info, message])
+
+    subject.log(:info, message)
+  end
+
+  it 'uses a ring buffer internally' do
+    expect(subject.send(:messages)).to be_instance_of(::Tundra::RingBuffer)
+  end
+
+  context '#dump_to_logger' do
+    let(:log_double) { instance_double('::Tundra::Loggers::StandardLogger') }
+    let(:message)    { 'some random message' }
+    let(:severity)   { :warning }
+
+    it 'dumps stored messages into another logger' do
+      expect(log_double).to receive(:log).with(severity, message)
+
+      subject.log(severity, message)
+      subject.dump_to_logger(log_double)
+    end
+
+    it 'empties the internal buffer after dumping the messages' do
+      allow(log_double).to receive(:log).with(severity, message)
+      expect(subject.send(:messages)).to receive(:clear!)
+
+      subject.log(severity, message)
+      subject.dump_to_logger(log_double)
+    end
+  end
 end
