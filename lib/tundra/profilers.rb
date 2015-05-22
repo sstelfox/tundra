68f432404eb0d7418183d49bfb8addc90e0e4bd9
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-05-21 20:40:07 -0500

    Playing with system stats

--- lib/tundra/profilers.rb
+++ lib/tundra/profilers.rb
@@ -1,4 +1,5 @@
 require 'tundra/profilers/cpu_profiler'
+require 'tundra/profilers/disk_profiler'
 
 module Tundra
   # All mechanisms that measure some performance or status metric about the
---
+++ lib/tundra/profilers/disk_profiler.rb
@@ -0,0 +1,27 @@
+module Tundra
+  module Profilers
+    class DiskProfiler
+      def line_data
+        stat_file_lines.map do |line|
+          key, value = line.split(': ')
+          [key, value.to_i]
+        end
+      end
+
+      def measure
+      end
+
+      def stat_file
+        "/proc/#{$$}/io"
+      end
+
+      def stat_file_lines
+        File.read(stat_file).split("\n")
+      end
+
+      def to_h
+        Hash[line_data]
+      end
+    end
+  end
+end
