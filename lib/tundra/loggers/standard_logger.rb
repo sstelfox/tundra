27287aaa6d8077a2d1b4561df06a2d0e030e4ed0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-05-22 12:53:25 -0500

    Inclusion is better

--- lib/tundra/loggers/standard_logger.rb
+++ lib/tundra/loggers/standard_logger.rb
@@ -10,9 +10,8 @@ module Tundra
     #     used as the output for all logged messages. Defaults to an instance
     #     of the standard Ruby Logger class.
     class StandardLogger
-      extend LevelLookup
-
       include ExceptionLogging
+      include LevelLookup
       include LevelShortcuts
       include LogOnce
 
@@ -29,7 +28,7 @@ module Tundra
       # @param [Symbol] severity The severity to log the message.
       # @param [String] message The message to get logged.
       def log(severity, message)
-        logger.log(self.class.severity_lookup(severity), message, LOG_NAME)
+        logger.log(severity_lookup(severity), message, LOG_NAME)
       end
 
       alias_method :add, :log
@@ -41,13 +40,13 @@ module Tundra
       #
       # @return [Symbol]
       def level
-        self.class.level_lookup[logger.level]
+        level_lookup[logger.level]
       end
 
       # Set the log level output to the provided level. If the new level is
       # invalid it will default to info.
       def level=(new_level)
-        logger.level = self.class.severity_lookup(new_level, :info)
+        logger.level = severity_lookup(new_level, :info)
       end
 
       # @!endgroup
--- spec/tundra/loggers/standard_logger_spec.rb
+++ spec/tundra/loggers/standard_logger_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe(Tundra::Loggers::StandardLogger) do
 
   it 'looks up the severity symbol using #severity_lookup' do
     expect(subject).to receive(:logger).and_return(log_double)
-    expect(described_class).to receive(:severity_lookup).and_return(-5)
+    expect(subject).to receive(:severity_lookup).and_return(-5)
     expect(log_double).to receive(:log).with(-5, 'msg', prog_name)
 
     subject.log(:stub, 'msg')
@@ -40,73 +40,65 @@ RSpec.describe(Tundra::Loggers::StandardLogger) do
 
   context '#level_lookup' do
     it 'returns a Hash' do
-      expect(described_class.send(:level_lookup)).to be_instance_of(Hash)
+      expect(subject.send(:level_lookup)).to be_instance_of(Hash)
     end
 
     it 'returns the :debug symbol when provided with the DEBUG constant' do
-      expect(described_class.send(:level_lookup)[::Logger::DEBUG]).to be(:debug)
+      expect(subject.send(:level_lookup)[::Logger::DEBUG]).to be(:debug)
     end
 
     it 'returns the :info symbol when provided with the INFO constant' do
-      expect(described_class.send(:level_lookup)[::Logger::INFO]).to be(:info)
+      expect(subject.send(:level_lookup)[::Logger::INFO]).to be(:info)
     end
 
     it 'returns the :warn symbol when provided with the WARN constant' do
-      expect(described_class.send(:level_lookup)[::Logger::WARN]).to be(:warn)
+      expect(subject.send(:level_lookup)[::Logger::WARN]).to be(:warn)
     end
 
     it 'returns the :error symbol when provided with the ERROR constant' do
-      expect(described_class.send(:level_lookup)[::Logger::ERROR]).to be(:error)
+      expect(subject.send(:level_lookup)[::Logger::ERROR]).to be(:error)
     end
 
     it 'returns the :fatal symbol when provided with the FATAL constant' do
-      expect(described_class.send(:level_lookup)[::Logger::FATAL]).to be(:fatal)
+      expect(subject.send(:level_lookup)[::Logger::FATAL]).to be(:fatal)
     end
 
     it 'returns the :unknown symbol when provided with the UNKNOWN constant' do
-      expect(described_class.send(:level_lookup)[::Logger::UNKNOWN])
-        .to be(:unknown)
+      expect(subject.send(:level_lookup)[::Logger::UNKNOWN]).to be(:unknown)
     end
   end
 
   context '#severity_lookup' do
     it 'returns the DEBUG constant when provided with :debug' do
-      expect(described_class.send(:severity_lookup, :debug))
-        .to eq(::Logger::DEBUG)
+      expect(subject.send(:severity_lookup, :debug)).to eq(::Logger::DEBUG)
     end
 
     it 'returns the INFO constant when provided with :info' do
-      expect(described_class.send(:severity_lookup, :info))
-        .to eq(::Logger::INFO)
+      expect(subject.send(:severity_lookup, :info)).to eq(::Logger::INFO)
     end
 
     it 'returns the WARN constant when provided with :warn' do
-      expect(described_class.send(:severity_lookup, :warn))
-        .to eq(::Logger::WARN)
+      expect(subject.send(:severity_lookup, :warn)).to eq(::Logger::WARN)
     end
 
     it 'returns the ERROR constant when provided with :error' do
-      expect(described_class.send(:severity_lookup, :error))
-        .to eq(::Logger::ERROR)
+      expect(subject.send(:severity_lookup, :error)).to eq(::Logger::ERROR)
     end
 
     it 'returns the FATAL constant when provided with :fatal' do
-      expect(described_class.send(:severity_lookup, :fatal))
-        .to eq(::Logger::FATAL)
+      expect(subject.send(:severity_lookup, :fatal)).to eq(::Logger::FATAL)
     end
 
     it 'returns the UNKNOWN constant when provided with :unknown' do
-      expect(described_class.send(:severity_lookup, :unknown))
-        .to eq(::Logger::UNKNOWN)
+      expect(subject.send(:severity_lookup, :unknown)).to eq(::Logger::UNKNOWN)
     end
 
     it 'returns the value of the UNKNOWN constant when provided a bad value' do
-      expect(described_class.send(:severity_lookup, 'what?'))
-        .to eq(::Logger::UNKNOWN)
+      expect(subject.send(:severity_lookup, 'what?')).to eq(::Logger::UNKNOWN)
     end
 
     it 'can return a provided constant when provided with a bad value' do
-      result = described_class.send(:severity_lookup, 'weooo', :info)
+      result = subject.send(:severity_lookup, 'weooo', :info)
       expect(result).to eq(::Logger::INFO)
     end
   end
